84396648f0ec89f2a249db9d3539ad51a5ff79b0
xabyer <xabyer.b@gmail.com>
2025-09-18 16:17:15 +0200

    added a read exercise.

--- UT1-Acceso_a_Ficheros/01-Gestion_de_Ficheros/JavaIO/1.-FicherosIO/FicherosIO/src/main/java/com/github/xabyer/dam/ad/Main.java
+++ UT1-Acceso_a_Ficheros/01-Gestion_de_Ficheros/JavaIO/1.-FicherosIO/FicherosIO/src/main/java/com/github/xabyer/dam/ad/Main.java
@@ -1,23 +1,43 @@
 package com.github.xabyer.dam.ad;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
     static void main() {
-        String text = IO.readln("Introduce a text: ");
-
-        try(
-                BufferedWriter writeText = new BufferedWriter(new FileWriter("text.txt"))
-        ) {
-            writeText.write(text + "\r\n");
-
+        try {
+            writeTextToFile();
+            readTextFromFile();
         } catch (IOException e) {
             e.printStackTrace(System.err);
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, e);
         }
     }
+
+    //Exercise 1
+    // Request a text message from the console and write it to a text file. Use BufferedWriter to do this.
+    public static void writeTextToFile() throws IOException {
+        String text = IO.readln("Introduce a text: ");
+
+        BufferedWriter writeText = new BufferedWriter(new FileWriter("text.txt", true));
+        writeText.write(text + "\r\n");
+
+        writeText.close();
+    }
+
+    //Exercise 2
+    // Read ebook.txt using BufferReader
+    public static void readTextFromFile() throws IOException {
+
+        BufferedReader readEbookList = new BufferedReader(new FileReader("ebookList.txt"));
+        String line = "";
+        while ((line = readEbookList.readLine()) != null) {
+            IO.println(line);
+        }
+
+        readEbookList.close();
+    }
 }
+
+
